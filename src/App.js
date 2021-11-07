1dc1b9396d60cc4a28039e3d849540afeda4815d
Guy Mathieu <mathieu.guye@gmail.com>
2021-11-07 10:19:02 -0500

    Created Component Directory

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
+import Directory from "./components/DirectoryComponents";
+import { Navbar, NavbarBrand } from 'reactstrap';
+
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <Navbar dark color="primary">
+        <div className="container">
+            <NavbarBrand href="/">NuCamp</NavbarBrand>
+        </div>
+      </Navbar>
+      <Directory  />
+  </div>;
 }
 
 export default App;
